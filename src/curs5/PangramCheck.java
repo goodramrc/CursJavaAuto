8ddc3425951374a6bf3e4cce85aa0868b8d621ef
mariusrc <maryusrosca@gmail.com>
2024-03-18 18:30:09 +0200

    Curs5 corectat

--- src/curs5/PangramCheck.java
+++ src/curs5/PangramCheck.java
@@ -1,24 +1,20 @@
 package curs5;
 
-public class PangramCheck {
-
-    package curs5;
-
 import java.util.Scanner;
 
-    /*
-     * facem un program care verifica daca un text este pangram
-     */
+/*
+ * facem un program care verifica daca un text este pangram
+ */
 
-    public class PangramCheck {
+public class PangramCheck {
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            //Scanner scan =  new Scanner(System.in);
-            String text = "The quick brown fo jumps over the lazy dog.".toLowerCase();
-            //		   01234....
-            //text.indexOf(0)
-            //	System.out.println(text.indexOf('h'));
+        //Scanner scan =  new Scanner(System.in);
+        String text = "The quick brown fo jumps over the lazy dog.".toLowerCase();
+        //		   01234....
+        //text.indexOf(0)
+        //	System.out.println(text.indexOf('h'));
 
 
 	/*	char litera = 'a';
@@ -29,24 +25,20 @@ import java.util.Scanner;
 
 			System.out.println(++litera);
 		}*/
-            boolean estePanagram = true;
+        boolean estePanagram = true;
 
-            for(char ch= 'a'; ch <= 'z'; ch++ ) {
+        for (char ch = 'a'; ch <= 'z'; ch++) {
 
-                if(text.indexOf(ch) == -1) {
-                    estePanagram = false;
-                    break;
-                }
-
+            if (text.indexOf(ch) == -1) {
+                estePanagram = false;
+                break;
             }
-
-
-            System.out.println(estePanagram? "Textul este panagram" : "Textul nu este panagram");
-
-
 
         }
 
-    }
 
+        System.out.println(estePanagram ? "Textul este panagram" : "Textul nu este panagram");
+
+
+    }
 }
