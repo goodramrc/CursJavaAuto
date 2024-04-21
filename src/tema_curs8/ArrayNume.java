20b5f4e26951e2f1b33326e8a34c2f1aa07f919a
mariusrc <maryusrosca@gmail.com>
2024-04-21 22:55:38 +0300

    tema_curs8_corectata

--- src/tema_curs8/ArrayNume.java
+++ src/tema_curs8/ArrayNume.java
@@ -4,17 +4,22 @@ import java.util.HashSet;
 
 public class ArrayNume {
 
-    String [] nume = {"Maria", "Bob", "Ion", "George", "Oana",
-            "Bogdan", "Oana", "Ion"};
+    String [] nume = {"Oana","Oana","Maria","Maria", "Bob", "Ion", "George", "Oana",
+            "Bogdan", "Oana", "Ion","Oana"};
 
     public void findDuplicateNames () {
 
         HashSet<String> nameList = new HashSet<String>();
+        HashSet<String> duplicateNames = new HashSet<String>();
 
         for (String numeDuplicat : nume) {
-            if(!nameList.add(numeDuplicat)) {
-                System.out.println("Numele duplicat este: " + numeDuplicat);
+            if (!nameList.add(numeDuplicat)) {
+                duplicateNames.add(numeDuplicat);
             }
+        }
+
+        for (String duplicateNameList : duplicateNames) {
+            System.out.println("Numele duplicat este: " + duplicateNameList);
         }
     }
 }
